9c6d414ec3b76d1e0f6cc317fb2cc5f4a00e3d32
phanrahan <myemail@stanford.edu>
2025-01-25 08:26:31 -0800

    Update make_stl.py

--- scripts/make_stl.py
+++ scripts/make_stl.py
@@ -2,9 +2,7 @@ import openscad
 
 scad = "JarAndCap.scad"
 
-#for d, h in [(30, 8), (50, 8)]:
-#for d, h in [(30, 8)]:
-for d, h in [(50, 8)]:
+for d, h in [(30, 8), (50, 8)]:
     openscad.run(scad, f"cap_{d}.stl", diameter=d, height=h, JAR=0)
     openscad.run(scad, f"jar_{d}.stl", diameter=d, height=h, JAR=1)
 
